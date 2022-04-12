c1a54ca8d781240b9cee920e5e049f679339f55c
Limgayoung <ygy920@gmail.com>
2022-04-12 20:50:29 +0900

    Greedy

--- Greedy/ThisIsCote_311.cpp
+++ Greedy/ThisIsCote_311.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <stdio.h>
+#include <algorithm>
 
 using namespace std;
 
@@ -19,23 +20,19 @@ int main() {
         scanf("%d", &n[i]);
     }
 
+    sort(n, n + N);
+
     j = 0;
-    for (i = 0; i < N-1; i++) {
-        j++;
-        if (count < n[i]) { //처음, 세는 중간에 더 큰 수가 들어오는 경우
-            count = n[i];
-        }       
-        if (j == count) {
+    for (i = 0; i < N; i++) {
+        count++;
+
+        if (count >= n[i]) {
             answer++;
-            count = n[i+1];
-            j = 0;
-        }        
+            count = 0;
+        }
+
     }
 
-    if (j+1 == count && n[i] <= count) {
-        answer++;
-    }
-   
     printf("%d", answer);
     
     return 0;
