dd240a9c6ffdd801e1ae2f361d51ce79dd1c606a
LimGayoung <ygy920@gmail.com>
2023-02-21 20:13:33 +0900

    Update ThisIsCote_262.cpp

--- Shortest Path/ThisIsCote_262.cpp
+++ Shortest Path/ThisIsCote_262.cpp
@@ -10,72 +10,64 @@ using namespace std;
 
 #define MAX_INT 2000000000
 
-vector<pair<int, int>> g[30001]; //연결된 정보 : node, time
-int dist[30001]; //각 노드마다 가는데 걸리는 시간 (최단경로)
-int isVisit[30002] = { 0 }; //방문 안한 곳이면 0
-int maxTime = 0; //총 걸리는 시간
-int cnt = 0; //방문한 도시 개수
+int N, M, C;
+vector<pair<int, int>> g[30001]; //연결된 node의 node 번호와 걸리는 time
+int dist[30002]; //C부터 n까지 도달하는 데 걸리는 시간 -> MAX로 초기화 해줘야 함
 
-void dijk(int start) {
-
-	//시간이 오래 걸리는 순서대로 방문해야 함
-	priority_queue<pair<int, int>> pq; //time, node
+void dijk(int start) {//각 n까지의 최소 시간을 다익스트라를 통해 저장
+	priority_queue<pair<int, int>> pq; //time, node -> time을 기준으로 정렬돼야 해서	
+	int nowNode, nowTime;
+	int i;
 
 	pq.push(make_pair(0, start));
-
-	int nowNode;
-	int nowTime;
-	int i;
+	dist[start] = 0; //시작점에서 시작점까지 걸리는 시간은 0
 
 	while (!pq.empty()) {
-		nowNode = pq.top().second;
-		nowTime = pq.top().first; //지금까지 걸린 시간		
+		nowNode = pq.top().second; //현재 노드
+		nowTime = pq.top().first; //지금까지 걸린 시간
 		pq.pop();
-		if (nowTime > dist[nowNode]) { //dist[nowNode]의 값이 지금까지 걸린 시간보다 작으면 이미 처리된 것
-			continue;
-		}
-		
-		for (i = 0; i < g[nowNode].size(); i++) {
-			//들어가는 비용 = 현재 노드에서 다음 노드까지 가는데 걸리는 시간 + 현재까지 걸린 시간
-			int cost = g[nowNode][i].second + nowTime;
-			if (cost < dist[g[nowNode][i].first]) { //start에서 다음 노드로 바로 가는것보다 현재 노드를 거쳐서 가는게 더 빠를 경우 dist 변경해주기
-				dist[g[nowNode][i].first] = cost;
-				//printf("push cost: %d node: %d\n", cost, g[nowNode][i].first);
-				pq.push(make_pair(cost, g[nowNode][i].first));
 
-			}
-		}
+		//printf("nowNode: %d nowTime: %d nowDist: %d\n", nowNode, nowTime, dist[nowNode]);
+		if (nowTime > dist[nowNode]) continue; //이미 보장된 노드 //이 조건이 없다면 양방향일 경우 영원히 탐색함
 
+		for (i = 0; i < g[nowNode].size(); i++) { //연결된 모든 노드 탐색
+			int nextNode = g[nowNode][i].first;
+			int cost = g[nowNode][i].second + nowTime; //다음 노드까지 걸리는 시간은 nowNode에서 다음 노드까지 걸리는 시간 + nowNode까지 걸린 시간
+			if (cost < dist[nextNode]) dist[nextNode] = cost; //nowNode를 거쳐서 nextNode로 가는 시간 < C에서 바로 nextNode로 가는 시간 인 경우 dist값 수정
+			//printf("nowNode: %d nextNode: %d cost: %d dist[nextNode]: %d\n", nowNode, nextNode, cost, dist[nextNode]);
+
+			pq.push(make_pair(dist[nextNode], nextNode));
+		}
 	}
 
-}
+} 
 
 int main() {
-
-	int N, M, C;
+	int X, Y, Z;
 	int i;
-	int a, b, w;	
+	int cnt = 0, time=0;
 
-	scanf("%d %d %d", &N, &M, &C);
+	scanf("%d %d %d", &N, &M, &C); //도시 개수, 통로 개수, 시작점
+
 	for (i = 0; i < M; i++) {
-		scanf("%d %d %d", &a, &b, &w);
-		g[a].push_back(make_pair(b, w));
+		scanf("%d %d %d", &X, &Y, &Z);
+		g[X].push_back(make_pair(Y, Z));
 	}
-
-	fill(dist, dist + 30001, MAX_INT);
+	
+	fill(dist, dist + N+1, MAX_INT);
 
 	dijk(C);
 
 	for (i = 1; i <= N; i++) {
 		if (dist[i] != MAX_INT) {
 			cnt++;
-			if (maxTime < dist[i]) maxTime = dist[i];
+			if (dist[i] > time) {
+				time = dist[i];
+			}
 		}
 	}
 
-	printf("%d %d", cnt, maxTime);
-	
-
+	printf("%d %d", cnt-1, time); //start는 제외하고 세야 함
 }
 
 /*
