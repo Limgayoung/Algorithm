d9d268286d6b5cf622470696f6f900f36bc054ff
LimGayoung <ygy920@gmail.com>
2024-02-01 16:22:22 +0900

    Update 2023.java

--- DFSBFS/2023.java
+++ DFSBFS/2023.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class 2023 {
+public class Swea {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
